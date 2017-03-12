d7d1cbbe9fe192ee79d91c423d0887589ef69962
priyankGh <ghorecha.priyank@hotmail.com>
2017-03-12 22:39:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict'
+
+    angular.module('myFirstAssignModule', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.menuItems = "";
+        $scope.resultMsg = "";
+        $scope.checkIfTooMuch = function () {
+            var arrayOfMenuItems = $scope.menuItems.split(',');
+            var size = arrayOfMenuItems.length;
+            if (size == 1 && arrayOfMenuItems[0].length == 0) {
+                $scope.resultMsg = "Please enter data first";
+            }
+            else if (size <= 3) {
+                $scope.resultMsg = "Enjoy!";
+            }
+            else {
+                $scope.resultMsg = "Too much!";
+            }
+        };
+    }
+})();
